0bada2936fd336a3d5514006dcf0f801b34a747c
Elliott Vargo <vargoelliott@gmail.com>
2021-01-22 02:43:39 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -17,7 +17,7 @@ def bonus
         {name: "Steven", age: "30", attitude: "confused"},
         {name: "Nurse", age: "44", attitude: "worried"}
     ]
- }
+ }}
 
         epic_tragedy[:montague][:hero][:status] = "dead"
 
